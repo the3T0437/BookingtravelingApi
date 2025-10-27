4042ed8481f558611ab78e64918096d88bc439f9
trongtinh <trongtinhkt@gmail.com>
2025-10-27 15:31:56 +0700

    avtuser

--- BookingTravelApi/Extensions/UserCompletedScheduleExtension.cs
+++ BookingTravelApi/Extensions/UserCompletedScheduleExtension.cs
@@ -14,7 +14,8 @@ namespace BookingTravelApi.Extensions
                 StartDate = userCompletedSchedule.Schedule!.StartDate,
                 EndDate = userCompletedSchedule.Schedule!.EndDate,
                 Name = userCompletedSchedule.User!.Name,
-
+                AvatarPath = userCompletedSchedule.User!.AvatarPath,
+                
                 // Loc userCompleted vs booking 
                 Booking = userCompletedSchedule.Schedule!.Bookings!.Where(b => b.UserId == userCompletedSchedule.UserId).Select(b => b.Map()).ToList()
             };
