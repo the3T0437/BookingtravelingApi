068a96317d0c652066e58bb4c9cb18a8ce16bdef
nppnam05 <nppnam05@gmail.com>
2025-11-10 13:08:11 +0700

    update UserController

--- BookingTravelApi/DTO/UpdatePassword/UpdatePassword.cs
+++ BookingTravelApi/DTO/UpdatePassword/UpdatePassword.cs
@@ -2,7 +2,6 @@ namespace BookingTravelApi.DTO.updatePassword
 {
     public class UpdatePassword
     {
-        public string email { get; set; } = null!;
         public string oldPassword { get; set; } = null!;
 
         public string newPassword { get; set; } = null!;
