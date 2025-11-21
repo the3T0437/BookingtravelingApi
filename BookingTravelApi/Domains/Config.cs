03a9e2e09c3cf7f3974eb14ea6d02731bc23c3c1
the3t <thanhthuong12032005@gmail.com>
2025-11-21 15:59:18 +0700

    staging

--- BookingTravelApi/Domains/Config.cs
+++ BookingTravelApi/Domains/Config.cs
@@ -1,11 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.ConstrainedExecution;
 
 namespace BookingTravelApi.Domains
 {
     [Table("configs")]
     public class Configs
     {
+        public static readonly int CountChangeSchedule = 1;
+        public static readonly int TimeExpiredBookingHour = 2;
+        public static readonly int TimeExpiredOtpMinutes = 3;
+
         [Key]
         [Required]
         public int Id  { get; set; }
